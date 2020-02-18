0af169dab7d6936961aa87a2c7326a017d091159
Yure <yure999@yahoo.es>
2020-02-18 10:38:47 +0100

    terminado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let containerElement = document.querySelector('.container');
+let containerElement = document.querySelector('.row');
 
 const apiURL = "https://randomuser.me/api/?results=8";
 
@@ -19,34 +19,35 @@ function fetchData(url) {
 
 fetchData(apiURL);
 
-function addCard(response){
+function addCard(response) {
     let result = response.results;
     for (let i = 0; i < result.length; i++) {
         let name = result[i].name;
-        let completeName = name.title +" "+ name.first +" "+ name.last;
+        let completeName = name.title + " " + name.first + " " + name.last;
         let image = result[i].picture.medium;
         let phoneNumber = result[i].phone;
         let userName = result[i].login.username;
         let email = result[i].email;
         let date = result[i].dob.date;
+        let finalDate =(date.substring(8,10) + "-" + date.substring(5,7) + "-" + date.substring(0,4));
         let location = result[i].location;
         let street = location.street.number + " " + location.street.name;
         let completeLocation = street + " " + location.city + " " + location.state;
 
         containerElement.innerHTML += `
             <div class="col-12 col-md-6 userCard">
-                <div class="image">
-                   <img src="${image}">
+                <div class="imageContainer">
+                   <img src="${image}" class="image">
                 </div>
                 <div class="infoContainer">
                     <div class="name">${completeName}</div>
                     <div class="phoneAndUser">
-                        <div class="phone">${phoneNumber}</div>
-                        <div class="userName">${userName}</div>
+                        <div class="phone"><i class="fas fa-phone"></i> ${phoneNumber}</div>
+                        <div class="userName"><i class="fas fa-user"></i> ${userName}</div>
                     </div>
-                    <div class="email">${email}</div>
-                    <div class="dob">${date}</div>
-                    <div class="completeLocation">${completeLocation}</div>
+                    <div class="email"><i class="fas fa-envelope"></i> ${email}</div>
+                    <div class="dob"><i class="fas fa-birthday-cake"></i> ${finalDate}</div>
+                    <div class="completeLocation"><i class="fas fa-map-marked-alt"></i> ${completeLocation}</div>
                 </div>
         </div>
         `
